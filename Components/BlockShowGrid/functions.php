d42ecc503b9cb97fec139341637ca0c1db76aa4d
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2021-10-22 13:17:47 +0200

    shows listing page + episode related show header

--- Components/BlockShowGrid/functions.php
+++ Components/BlockShowGrid/functions.php
@@ -23,23 +23,13 @@ function getACFLayout()
         'name' => 'BlockShowGrid',
         'label' => 'Block: Show Grid',
         'sub_fields' => [
-            // [
-            //     'label' => __('Date', 'flynt'),
-            //     'name' => 'titleTab',
-            //     'type' => 'tab',
-            //     'placement' => 'top',
-            //     'endpoint' => 0
-            // ],
-            // [
-            //     'label' => __('Month', 'flynt'),
-            //     'name' => 'title',
-            //     'type' => 'date_picker',
-            //     'required' => 0,
-            //     'display_format' => 'm.Y',
-            //     'return_format' => 'm.Y',
-            //     'first_day' => 1,
-            //     'step' => 1,
-            // ],
+            [
+                'label' => __('Title', 'flynt'),
+                'name' => 'title',
+                'type' => 'text',
+                'required' => 0,
+                'step' => 1,
+            ],
             [
                 'label' => __('Episodes', 'flynt'),
                 'name' => 'contentSelectionTab',
@@ -57,30 +47,6 @@ function getACFLayout()
                 'ui' => 1,
                 'required' => 0,
             ],
-            // [
-            //     'label' => __('Options', 'flynt'),
-            //     'name' => 'optionsTab',
-            //     'type' => 'tab',
-            //     'placement' => 'top',
-            //     'endpoint' => 0
-            // ],
-            // [
-            //     'label' => '',
-            //     'name' => 'options',
-            //     'type' => 'group',
-            //     'layout' => 'row',
-            //     'sub_fields' => [
-            //         [
-            //             'label' => __('Columns', 'flynt'),
-            //             'name' => 'columns',
-            //             'type' => 'number',
-            //             'default_value' => 3,
-            //             'min' => 1,
-            //             'max' => 4,
-            //             'step' => 1
-            //         ],
-            //     ]
-            // ]
         ]
     ];
 }
